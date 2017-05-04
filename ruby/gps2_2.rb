e113b1d872aab9f50f11d801d52e4929d3496f80
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-03 17:09:07 -0700

    creat list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,43 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# making an array
+# set hash with items and quantity place holder
+# steps: 
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+def creat_list(list)
+	
+	grocery_list = list.split(" ")
+
+end
+p creat_list("arrots apples cereal pizza")
+
+
+
+# def hash_lists(hash_list, item, quantity)
+# 	hash_list[item] = quantity
+# end
+
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
