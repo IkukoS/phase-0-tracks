1293e13cfba34f0979dea1f46af1d64c50443c87
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-04 06:54:25 -0700

    updated the file with the comments for the questions.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -18,32 +18,63 @@ def creat_list(list)
 	hash_list
 	
 end
-p creat_list("arrots apples cereal pizza")
+new_list = creat_list("carrots apples cereal pizza")
 
 
 
 
-# def hash_lists(hash_list, item, quantity)
-# 	hash_list[item] = quantity
-# end
 
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
-# steps:
-# output:
+# steps: take an item and quantity add to hash list
+# output: new hash list
+
+
+
+
+
+def add_item(hash_list, item, quantity)
+	hash_list[item] = quantity
+	hash_list
+end
+
+p add_item(new_list, "potato", 3)
+
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: item to be remove
+# steps: delete an item from the hash 
+# output: hash without the item
+
+def delete_item(hash_list, item)
+	hash_list.delete(item)
+	hash_list
+end
+
+p delete_item(new_list, "apples")
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: take a hash list and quantity update
+# steps: hash list with a new quantity
+# output: update hash list
+
+def update_quantity(hash_list, item, quantity)
+	hash_list[item] = quantity
+	hash_list
+end
+
+p update_quantity(new_list, "pizza", 2)
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: hash list 
+# steps: iterate over has list
+# output: hash list
+
+def pretty_list(hash_list)
+	hash_list.each do |item, quantity|
+		puts "#{item} : #{quantity}"
+	end
+end
+
+p pretty_list(new_list)
