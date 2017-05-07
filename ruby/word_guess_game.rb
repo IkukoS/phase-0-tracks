b0a8855aa2765b95ccb28aa090768dc8dabe4fa3
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-07 15:11:30 -0700

    6.7

---
+++ ruby/word_guess_game.rb
@@ -0,0 +1,11 @@
+class Word_guessing
+end
+
+def take_original_word(original)
+    puts "Enter a word to be guessed."
+    @original_word = original
+end
+
+def letter_number
+	@original_word.length = @times
+end
