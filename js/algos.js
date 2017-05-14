4fbff89b62761f26c3d09876c56e3999f4436732
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-14 16:17:51 -0700

    added a file made an array and picked the first item.

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// Find out the longest word or phrase in the array.
+// count how many letters in each word
+// pick the longest word or phrase then print it.
+
+// pick an item from the array
+array = ["long phrase","longest phrase","longer phrase"]
+console.log(array[0]);
+
