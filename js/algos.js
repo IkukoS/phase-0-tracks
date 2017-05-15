3f96c2967dc719ba4b0946cb75be6840831d5006
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-14 18:31:19 -0700

    made the rules as a function so that I can use it for different arraies.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-// Find out the longest word or phrase in the array.
+/* Find out the longest word or phrase in the array.
 // count how many letters in each word
 // pick the longest word or phrase then print it.
 
@@ -16,7 +16,40 @@ var array = ["long phrase","longest phrase","longer phrase"]
 // use loop for checking the lenght of the phrase or word
 // first, print all the element length
 
-for (var i = 0; i < array.length; i++) {
-	console.log(array[i].length);
+// How to find difine which is the longest in the array and print as a result??
+// deifine the items length as a valiable so that I can compare the next one.
+// also define the longest item so that I can call it later.
+var item_length = 0;
+var longest;
+
+for(var i=0; i < array.length; i++){
+    if(array[i].length > item_length){
+        var item_length = array[i].length;
+        longest = array[i];
+    }      
+} 
+
+console.log(longest);*/
+
+
+
+//make the rule I found out as a function so that I can use other arraies
+
+function longest_word(array){
+
+var item_length = 0;
+var longest;
+
+for(var i=0; i < array.length; i++){
+    if(array[i].length > item_length){
+         item_length = array[i].length;
+        longest = array[i];
+    }      
+} 
+
+console.log(longest);
 
 }
+
+longest_word(["long phrase","longest phrase","longer phrase"]);
+
