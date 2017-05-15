b70decb14d7bb6a1e2715576ef5a1f0796922661
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-14 17:14:05 -0700

    changed the way to print the length to 'for' instaed of useing forEach

--- js/algos.js
+++ js/algos.js
@@ -3,18 +3,20 @@
 // pick the longest word or phrase then print it.
 
 // pick an item from the array
-array = ["long phrase","longest phrase","longer phrase"]
+var array = ["long phrase","longest phrase","longer phrase"]
 // print the wrod or phrase
-console.log(array[0]);
+// This test worked fine .......  console.log(array[0]);
 
 // Check howlong the first item is.
-console.log(array[0].length);
+// This test worked fine .......  console.log(array[0].length);
 
 // Do samethings for the next item and if the length is larger than the last one print the word instaed .
 // If the first item is longer than that judt keep printing same one.
 
 // use loop for checking the lenght of the phrase or word
 // first, print all the element length
-array.forEach(function(element){
-  console.log(element.length);
-})
+
+for (var i = 0; i < array.length; i++) {
+	console.log(array[i].length);
+
+}
