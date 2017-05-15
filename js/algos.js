d90acf6dc9916797a712c821a14ec79f14774ccf
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-14 17:01:54 -0700

    printed out each items length

--- js/algos.js
+++ js/algos.js
@@ -4,5 +4,17 @@
 
 // pick an item from the array
 array = ["long phrase","longest phrase","longer phrase"]
+// print the wrod or phrase
 console.log(array[0]);
 
+// Check howlong the first item is.
+console.log(array[0].length);
+
+// Do samethings for the next item and if the length is larger than the last one print the word instaed .
+// If the first item is longer than that judt keep printing same one.
+
+// use loop for checking the lenght of the phrase or word
+// first, print all the element length
+array.forEach(function(element){
+  console.log(element.length);
+})
