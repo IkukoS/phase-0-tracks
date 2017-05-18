4e3be7018a50560a7564fc708d6c156c313f4d10
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-17 23:03:20 -0700

    found the all culprit

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,36 +1,40 @@
+
 # Replace in the "<???>" with the appropriate method (and arguments, if any).
 # Uncomment the calls to catch these methods red-handed.
 
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".replace"zoom"
+p "zom".replace("zoom")
 # => “zoom”
 
-"enhance".center(20)
+p "enhance".center(20)
 # => "    enhance    "
 
-"Stop! You're under arrest!".upcase
+p "Stop! You're under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".+" suspects"
+p "the usual" << " suspects"
 #=> "the usual suspects"
 
-"The case of the disappearing last letter".chop
+p " suspects".prepend("the usual")
+# => "the usual suspects"
+
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete"T"
+p "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".strip
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-"z".gsub(/./) {|s| s.ord.to_s + ' '}  
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-"How many times does the letter 'a' appear in this string?".count "a"
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
