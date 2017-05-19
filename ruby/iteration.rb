50db6f8ccd3d7dde97b557a81d5898e163f4cc0a
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-19 12:49:38 -0700

    made a hash and iterate

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,6 @@
 fish = ["tuna", "salmon", "bass", "sea_urchin", "eel"]
 
-sakana =[]
+sakana =["maguro", "sake", "basu", "uni", "unagi"]
 
 puts "before .each call"
 p fish
@@ -14,3 +14,18 @@ puts "after .each call"
 p fish
 p sakana
 
+fish_sakana = {
+ 	tuna:  -"maguro",
+    salmon: "sake",
+    bass: "basu",
+    sea_urchin: "uni",
+    eel: "unagi"
+ }
+
+
+
+ fish_sakana.each do |f,s|
+   puts "#{s} is japanese name of #{f}"
+end
+
+	
