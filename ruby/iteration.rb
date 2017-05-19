a903ecb55b17c4f1e70666aca2eb59a2e6964b77
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-19 12:07:17 -0700

    modified a new array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,7 +1,16 @@
 fish = ["tuna", "salmon", "bass", "sea_urchin", "eel"]
 
+sakana =[]
+
+puts "before .each call"
 p fish
+p sakana
 
 fish.each do |fish|
-	puts fish 
+    sakana << fish
 end
+
+puts "after .each call"
+p fish
+p sakana
+
