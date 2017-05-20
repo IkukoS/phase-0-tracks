180c451635a0d18a5a6e6213296821880f13e46e
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-20 00:02:42 -0700

    used .map! method for the array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,31 +1,40 @@
+# making an array with fish names
+
 fish = ["tuna", "salmon", "bass", "sea_urchin", "eel"]
 
-sakana =["maguro", "sake", "basu", "uni", "unagi"]
-
-puts "before .each call"
+# make sentenses with the fish names
 p fish
-p sakana
-
 fish.each do |fish|
-    sakana << fish
+    puts "I love #{fish}!"
 end
 
-puts "after .each call"
 p fish
-p sakana
+# change the items with -lover!!
+
+fish.map! do |fish|
+	fish + "_lover"
+end 
+
+# make sure if the items has been changed
+p fish
+
+# making a hash with fish and the japanese name ot each fish
 
 fish_sakana = {
- 	tuna:  -"maguro",
+ 	tuna: "maguro",
     salmon: "sake",
     bass: "basu",
     sea_urchin: "uni",
     eel: "unagi"
  }
 
+p fish_sakana
+# print the sentence that tell us the Japanese way of fish
 
-
- fish_sakana.each do |f,s|
-   puts "#{s} is japanese name of #{f}"
+ fish_sakana.each do |fish,sakana|
+   puts "#{sakana} is japanese name of #{fish}"
 end
+
+p fish_sakana
 
 	
