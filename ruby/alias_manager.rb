5efad5407cebfdb7cd2b48925814c273f39f3725
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-20 22:21:10 -0700

    ade an array of letters with the first item of the recersed_name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,11 +1,12 @@
 # write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name 
 def alias_name(name)
 
-# change the name to an array with two items
+# change the name to an array with two items then swith the items.
  	 array_name = name.split(' ')
 # switch the items place
- 	 p array_name.reverse!
-
- 	 
+ 	 reversed_name = array_name.reverse!
+# woking one the first item only. Pidk the first word and make it letters.
+	p reversed_name[0].split('')
+end	 
 
 alias_name("Felicia Torres")
