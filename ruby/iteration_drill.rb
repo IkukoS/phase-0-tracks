1d13d083b45120542971f1b60915bf0327716d7b
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-21 14:22:43 -0700

    added array question 1, 3, 4

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -6,20 +6,74 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
 # ----
+zombie_apocalypse_supplies.map! do |item|
+   item + " * "
+ end
+ p zombie_apocalypse_supplies.join
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
 
+loop do
+    changed = false
+
+    num_do.times do |x|
+      if zombie_apocalypse_supplies[x].downcase > zombie_apocalypse_supplies[x+1].downcase
+        
+        zombie_apocalypse_supplies[x],zombie_apocalypse_supplies[x+1] = zombie_apocalypse_supplies[x+1],zombie_apocalypse_supplies[x]
+        
+        changed = true
+      end
+    end
+
+  break if not changed
+
+end
+
+p zombie_apocalypse_supplies
+
+
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
+def alreacy_have(checking_item)
+
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+
+  zombie_apocalypse_supplies.each do |item|
+    if item == checking_item
+      puts "We already have #{item}."
+    end
+  end
+end
+
+  alreacy_have("shotgun")
+  alreacy_have("water")
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
+def pick_five
+  zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+
+  chosen_item = []
+    while chosen_item.length < 5
+      picked = zombie_apocalypse_supplies.sample
+      chosen_item << picked
+      chosen_item.uniq
+    end
+  p chosen_item
+
+end
+
+pick_five
+
+
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -29,6 +83,26 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
+def add_items
+  zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+  other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
+                            "solar battery", "flashlight"]
+  index = 0
+  
+  zombie_apocalypse_supplies.each do |item|
+    while index < other_survivor_supplies.length
+      if other_survivor_supplies[index] == item
+        zombie_apocalypse_supplies << other_survivor_supplies[index]
+        
+      end
+      index += 1
+    end
+  end
+    p zombie_apocalypse_supplies
+end
+
+add_items
 
 # Hash Drills
 
