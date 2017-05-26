db7824c1029d63e072074d208bd2b627cc4e0923
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-26 09:21:29 -0700

    added hallily method and drivercode

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,11 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	words + "!!!" + " :)"
+  end
 end
+
+p Shout.yell_angrily("Hey"), Shout.yelling_happily("Hey")
