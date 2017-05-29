7f738fad54fcea6c90106f2a5224a5f151b3eceb
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-29 02:25:16 -0700

    added tried array method

--- ruby/word_guess_game_spec.rb
+++ ruby/word_guess_game_spec.rb
@@ -1,9 +1,19 @@
 require_relative 'word_guess_game'
 
 describe WordGuessGame do 
-	let(:game) {WordGuessGame.new(original_word)}
+	let(:game) {WordGuessGame.new("happy")}
 
 	it "take a word and check the numbers of the letters" do
 		expect(game.empty_array).to eq "_ _ _ _ _ "
 	end
+
+	it "takes a guess word and give a feedback" do
+		expect(game.check_letters("angry")).to eq "_ a _ _ y "
+	end
+
+	it "store the guesses" do
+		game.check_letters("angry")
+		expect(game.tried_arr).to eq ["angry"]
+	end
+
 end 
