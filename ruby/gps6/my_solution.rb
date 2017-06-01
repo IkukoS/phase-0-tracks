5b2604bc6df14a91ba9b50fda443517b19b5b6da
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-05-31 21:21:44 -0700

    refoctor and added some comments

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -27,6 +27,10 @@ attr_reader :virus_effects
     @population = population
     @population_density = population_density
   end
+ 
+  # private 
+  # if I put private here I get error. under private you can not call a method.
+  # but if the private is under virus_effect method it still work even this passing methods under private
 
   # The virus_effects method looks like a getter for predicted_deaths and speed_of_spread.
   def virus_effects
@@ -39,17 +43,17 @@ attr_reader :virus_effects
   # This depends on the population density, establishing a positive correlation between population density and the number of deaths.
   def predicted_deaths#(population_density, population, state)
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    elsegit 
-      number_of_deaths = (@population * 0.05).floor
-    end
+    number_of_deaths = if @population_density >= 200
+									      (@population * 0.4).floor
+									    elsif @population_density >= 150
+									      (@population * 0.3).floor
+									    elsif @population_density >= 100
+									      (@population * 0.2).floor
+									    elsif @population_density >= 50
+									      (@population * 0.1).floor
+									    else
+									      (@population * 0.05).floor
+									    end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
@@ -61,21 +65,22 @@ attr_reader :virus_effects
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
-    end
+    speed +=  if @population_density >= 200
+                  0.5
+              elsif @population_density >= 150
+                  1
+              elsif @population_density >= 100
+                  1.5
+              elsif @population_density >= 50
+                  2
+              else
+                  2.5
+              end
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
   end
+
 
 end
 
@@ -104,3 +109,20 @@ STATE_DATA.each do |state, data|
 end
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+#  => and : 
+ 
+# What does require_relative do? How is it different from require?
+#  require_relative copy and pasting satte_data 
+#  require_relative = same directory, require needs full passway 
+
+# What are some ways to iterate through a hash?
+#  If the global scope is used evenif there are no hash pass in the current method directry, as long as the method included in the current method, has passway to the hash, it can iterate.
+#  or just use @ to connect to the hash. Then we can use methods like .each
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+#  It was amazing how we can simlpify the code. In the end we have only two method names!! As long as the things pass on is instance value, we even need not pass it!
+
+# What concept did you most solidify in this challenge?
+# private was something new and interesting. It is really clear you can not call the method if it is under private! 
