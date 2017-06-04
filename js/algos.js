f74f3f99052ad4083718bb9b4d956019f43d843a
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-06-04 15:49:18 -0700

    completed relese0

--- js/algos.js
+++ js/algos.js
@@ -1,21 +1,27 @@
-/*write a function that takes an array of words or phrases and returns 
+/*
+write a function that takes an array of words or phrases and returns 
 the longest word or phrase in the array. 
 
-make a loop to make a object which hold pairs with the word(or phrase) and the it's length.
-compare the length and pick a longest one
-   How to pcik a longest one?? 
-     compare the first one and next one ane if the first one is greater than second one
-     keep the fiest one otherwize keep the next one and so on...
-print the one till last
+take an array with some words or phrases
+set the first word as a longest word
+compare the word to other words one by one.
+if the other word is longer than the fist word assign the word as longest word.
+at the end print the longest word.
 
-added commetns */
+*/
 
-var array = ["long phrase","longest phrase","longer phrase"];
-//console.log(array);
-var match_length = {};
+function longestWordFinder(array) {
 
-for (var i = 0; i < array.length; i++) {
-	match_length[array[i]] = array[i].length;
+	var longestWord = array[0];
+
+	for (var i = 1; i < array.length; i++) {   
+		if(longestWord.length < array[i].length){
+				  longestWord = array[i];
+			}
+	}
+console.log(longestWord)  
+
 }
 
-console.log(match_length)
+longestWordFinder(["long phrase","longest phrase","longer phrase"])
+longestWordFinder(["Happy birthday!","Nice to meet you!","Today is Sunday."])
