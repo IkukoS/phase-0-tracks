b4c8318003ee243540aa7ed7a59c4b24de2425f7
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-06-05 08:40:55 -0700

    everything works fine on repl but keep getting errors

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,5 @@
-/*
-Relese 0
-write a function that takes an array of words or phrases and returns 
-the longest word or phrase in the array. 
 
-take an array with some words or phrases
-set the first word as a longest word
-compare the word to other words one by one.
-if the other word is longer than the fist word assign the word as longest word.
-at the end print the longest word.
 
-*/
-/*
 function longestWordFinder(array) {
 
 	var longestWord = array[0];
@@ -26,22 +15,10 @@ console.log(longestWord)
 
 longestWordFinder(["long phrase","longest phrase","longer phrase"])
 longestWordFinder(["Happy birthday!","Nice to meet you!","Today is Sunday."])
-*/
-/*
-Relese 1
-write a function that takes two objects and checks to see 
-if the objects share at least one key-value pair.
 
-Take two objecs 
-find out the keys words so that I can access to the values
-check if the sames keys value is same or not
-if they are same store ture to an array.
-if they are not same store false in the same array
-after checked all the keys check that if the array includes ture.
-if it includes true print true otherise orint false
-*/
 
-/*
+
+
 function keyValueMatch(data1, data2) {
 	var keys = []
 	  for (var key in data1) {
@@ -68,57 +45,36 @@ keyValueMatch({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3});
 keyValueMatch({animal: "Cat", legs: 4}, {animal: "Dog", legs: 3});  
 keyValueMatch({name: "Steven", age: 54, gender: "male"}, {name: "Tamir", age: 54, gender: "female"});
 keyValueMatch({name: "Steven", age: 54, gender: "male"}, {name: "Tamir", age: 29, gender: "female"});
-*/
 
 
 
-/*
-Relese 2
-Write a function that takes an integer for length,
-and builds and returns an array of strings of the given length
-make an array so that I can store the letters I generate
-How to make a rondom word => pick a rondomnumber one to 10
-console.log(Math.ceil(Math.random()*10))
-pick a romdon letter from alphabet array then store it an array romdom times.
-make the letters array in to a string and store the word to an array
 
 
-*/
 
-
-/*
 function wordGenerater(num) {
-	var array = [];
-
-for (var word = 0; word < num; word++){
-  	array.push(word);
+  var array = [];
+  for (var n = 0; n < num; n++) {
 
 	var letterArray = [];
 	var letters =
     ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   
-  		letterArray = []
 				for (var i = 0; i < Math.ceil(Math.random()*10); i++) {
 		  	letterArray.push(letters[Math.floor(Math.random()*letters.length)]);
 		  	word = letterArray.join("");
 		    }
-		  
-		  for (var i = 0; i < num; i++){
-  	array.push(word);
-}
-		    console.log(array)
-   }
-   
 
+  array.push(word);
   
-  
-  //array.push(picked)
-  
-  //console.log(words);
+  }
+
+ console.log(array);
+
 }
 
-wordGenerater(4)
-*/
+wordGenerater(4);
+
+
 
 
 
