5d21264d86502989d1857dc2b14b103035b31c8b
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-06-09 16:05:23 -0700

    can take the informaiton for personal table from users and store the table

--- databases/study_track/study_track.rb
+++ databases/study_track/study_track.rb
@@ -27,14 +27,19 @@ create_subject_table = <<-SQL
 db.execute(create_personal_table)
 db.execute(create_subject_table)
 
+def taking_data(db, name, start_day, last_day, personal_id)
+  db.execute("INSERT INTO personal (name, start_day, last_day, personal_id) VALUES (?, ?, ?, ?)", [name, start_day, last_day, personal_id])
+end
 
 # USER INTER FACE
 #provide a student id to the students.
 #store the student name and number in array
 
 student_name = ""
-student_id = 1
+student_id = 0
 student_names = []
+start_date = ""
+ending_date = ""
 until student_name == "done"
 	puts "Let's set your study target!"
 	puts "What is your name? If there are not more stuents enter 'done'."
@@ -43,8 +48,15 @@ until student_name == "done"
 			puts "Hi, #{student_name.capitalize}! Your student number is #{student_id}."
 			student_id += 1
       student_names << student_name.capitalize 
+      puts "When do you want to start your log? (ex. 6/10)"
+      start_date = gets.chomp
+      puts "When do you want to end your log? (ex. 6/10)"
+      ending_date = gets.chomp
+      taking_data(db, student_name, start_date, ending_date, student_id)
 		end
 	puts "-----------------------------------------------------------------"
+	
 end
-p student_names
+
+
 
