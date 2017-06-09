a3828b30f03bb9dfd68b72215442c2158f244fc6
Ikuko Schoeller <ikukoschoeller@gmail.com>
2017-06-09 16:39:00 -0700

    can get information of subject and store in the database

--- databases/study_track/study_track.rb
+++ databases/study_track/study_track.rb
@@ -27,9 +27,14 @@ create_subject_table = <<-SQL
 db.execute(create_personal_table)
 db.execute(create_subject_table)
 
-def taking_data(db, name, start_day, last_day, personal_id)
+def taking_personal_data(db, name, start_day, last_day, personal_id)
   db.execute("INSERT INTO personal (name, start_day, last_day, personal_id) VALUES (?, ?, ?, ?)", [name, start_day, last_day, personal_id])
 end
+
+def taking_subject_data(db, s_id, subject, target_hours)
+  db.execute("INSERT INTO subjects (s_id, subject, target_hours) VALUES (?, ?, ?)", [s_id, subject, target_hours])
+end
+
 
 # USER INTER FACE
 #provide a student id to the students.
@@ -40,6 +45,8 @@ student_id = 0
 student_names = []
 start_date = ""
 ending_date = ""
+target_subject = ""
+hours = ""
 until student_name == "done"
 	puts "Let's set your study target!"
 	puts "What is your name? If there are not more stuents enter 'done'."
@@ -47,12 +54,18 @@ until student_name == "done"
   	if student_name != "done"		
 			puts "Hi, #{student_name.capitalize}! Your student number is #{student_id}."
 			student_id += 1
-      student_names << student_name.capitalize 
       puts "When do you want to start your log? (ex. 6/10)"
       start_date = gets.chomp
       puts "When do you want to end your log? (ex. 6/10)"
       ending_date = gets.chomp
-      taking_data(db, student_name, start_date, ending_date, student_id)
+      taking_personal_data(db, student_name, start_date, ending_date, student_id)
+      puts "Which subject would you like to set a goal?"
+      target_subject = gets.chomp
+      puts "How many hours do you want to study #{target_subject} during #{start_date}-#{ending_date}?"
+      hours = gets.chomp.to_i
+      taking_subject_data(db, student_id, target_subject, hours)
+      puts "Would you like to set another goal for anothre subject?"
+      
 		end
 	puts "-----------------------------------------------------------------"
 	
